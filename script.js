aa85bb3c8cc637e1ff7d7cd44c1cabe4f1bc5c2c
mcher0 <mansuperman.ms@gmail.com>
2025-01-27 15:48:43 +0100

    created computer choice logic

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hiii!")
+function getComputerChoice(){
+    let choice = Math.random();
+    if (choice <= 0.33){
+        return "rock"
+    } else if(choice >= 0.33 && choice <=0.66){
+        return "paper"
+    } else{
+        return "scissors"
+    }
+}
+console.log(getComputerChoice());
